90ac5ed879ac52cbb7b827a3a20b7a518d2e9173
Veitchie <jonathonveitch83@gmail.com>
2024-07-19 12:30:08 +0100

    Added VL53L0X smoothsetup

---
+++ code/LIDAR.py
@@ -0,0 +1,11 @@
+import adafruit_vl53l0x
+import board
+import busio
+
+class VL53L0X(adafruit_vl53l0x.VL53L0X):
+    def __init__(self):
+        
+        i2c = busio.I2C(board.SCL, board.SDA)
+        print("i2c",i2c)
+
+        super(adafruit_vl53l0x.VL53L0X,self).__init__(i2c)
